681c7ae0c818666ae01714bd9c61d3adf97e22d8
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-09-27 22:28:09 +0800

    Added calculations for Current

--- Exercise 2 - Ohm’s Law Calculator/ohms_law_calculator.py
+++ Exercise 2 - Ohm’s Law Calculator/ohms_law_calculator.py
@@ -16,17 +16,32 @@ while True:
         while True:
             try:
                 current_value = float(input("Please enter the Current (I) in Amperes: "))
-                resistance_value = float(input("Please enter the Resistance (R) in Ohms: "))
+                resistance_value = float(input("Please enter the Resistance (R) in Ω: "))
                 if resistance_value == 0:
                     print("\nError! Resistance cannot be zero!\n")
                 else:
                     voltage_value = current_value * resistance_value
-                    print(f"The Voltage (V) is {voltage_value} volts")
+                    print(f"\nThe Voltage (V) is {voltage_value} volts")
                     break
             except ValueError:
                 print("\nInvalid Input! Please enter numeric values!\n")
 
     # Calculation for Current (I)
+    elif user_choice.upper() == 'I':
+        print("\nCalculation for Current (I)")
+        while True:
+            try:
+                voltage_value = float(input("Please enter the Voltage (V) in Volts: "))
+                resistance_value = float(input("Please enter the Resistance (R) in Ω: "))
+                if resistance_value == 0:
+                    print("\nError! Resistance and Division cannot be zero!\n")
+                else:
+                    current_value = voltage_value / resistance_value
+                    print(f"\nThe Current (I) is {current_value} Amperes")
+                    break
+            except ValueError:
+                print("\nInvalid Input! Please enter numeric values!\n")
+
     # Calculation for Resistance (R)
 
 # Display Result
