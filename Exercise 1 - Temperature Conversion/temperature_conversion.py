b66c146e98609a1da17f117b9c95d0b43273c905
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-09-27 22:18:34 +0800

    Added user codes and calculation for voltage

--- Exercise 1 - Temperature Conversion/temperature_conversion.py
+++ Exercise 1 - Temperature Conversion/temperature_conversion.py
@@ -19,17 +19,18 @@ while True:
     while True:
         # Ask the user to select conversion type from Celsius to Fahrenheit or from Fahrenheit to Celsius
         print("\nPlease select a conversion type")
-        conversion_type = input(f"Type {blue + "'C'" + green} to convert from {blue + "Celsius to Fahrenheit" + green} or type {blue + "'F'" + green} to convert from {blue + "Fahrenheit to Celsius"+ green}: ")
+        conversion_type = input(
+            f"Type {blue + "'C'" + green} to convert from {blue + "Celsius to Fahrenheit" + green} or type {blue + "'F'" + green} to convert from {blue + "Fahrenheit to Celsius" + green}: ")
 
         # Conversion and Output result
         if conversion_type.upper() == 'C':
             # Celsius to Fahrenheit
-            converted_celsius = (temp_value * 9/5) + 32
+            converted_celsius = (temp_value * 9 / 5) + 32
             print(yellow + f"\n{temp_value}° Celsius is {converted_celsius}° Fahrenheit.")
             break
         elif conversion_type.upper() == 'F':
             # Fahrenheit to Celsius
-            converted_fahrenheit = (temp_value -32) * 5/9
+            converted_fahrenheit = (temp_value - 32) * 5 / 9
             print(yellow + f"\n{temp_value}° Fahrenheit is {converted_fahrenheit}° Celsius.")
             break
         else:
--- Exercise 2 - Ohm’s Law Calculator/ohms_law_calculator.py
+++ Exercise 2 - Ohm’s Law Calculator/ohms_law_calculator.py
@@ -5,10 +5,30 @@
 # Exercise 2: Ohm’s Law Calculator
 # Create a program that calculate Voltage, Current, or Resistance by using Ohm's Law
 
-# Zero Division Rule
 # Ask the user what they want to calculate. Either Voltage, Current, or Resistance
-# Ask the user to input values
-# Use Ohm's Law to calculate missing variable
+while True:
+    print("\nWelcome to the Ohm's Law Calculator!")
+    user_choice = input("Please enter 'V' to calculate Voltage,'I' for Current, and 'R' for Resistance: ")
+# Ask the user to input values, Use Ohm's Law to calculate missing variable
+    # Calculation for Voltage (V)
+    if user_choice.upper() == 'V':
+        print("\nCalculation for Voltage (V)")
+        while True:
+            try:
+                current_value = float(input("Please enter the Current (I) in Amperes: "))
+                resistance_value = float(input("Please enter the Resistance (R) in Ohms: "))
+                if resistance_value == 0:
+                    print("\nError! Resistance cannot be zero!\n")
+                else:
+                    voltage_value = current_value * resistance_value
+                    print(f"The Voltage (V) is {voltage_value} volts")
+                    break
+            except ValueError:
+                print("\nInvalid Input! Please enter numeric values!\n")
+
+    # Calculation for Current (I)
+    # Calculation for Resistance (R)
+
 # Display Result
 
 # End
