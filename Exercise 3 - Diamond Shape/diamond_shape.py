dca6b1551041b3643c48382e1d48689a7422b9f7
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-09-27 23:35:42 +0800

    Added pseudocodes for documentation

--- Exercise 3 - Diamond Shape/diamond_shape.py
+++ Exercise 3 - Diamond Shape/diamond_shape.py
@@ -5,3 +5,10 @@
 # Exercise 3: Diamond Shape
 # Write a Python function named print_diamond that takes an odd integer n as an argument and prints a diamond shape with a width of n using the * character.
 
+# Function for printing a diamond pattern of width n using '*' characters.
+# Checker if n is odd
+# Prints the top part of the diamond
+# Prints the bottom part of the diamond
+# Ask the user for input of odd integer n
+# End
+
