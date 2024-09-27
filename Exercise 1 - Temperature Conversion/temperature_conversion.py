d5463979e3f68e5d8883059584aa2f746072603e
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-09-27 21:19:09 +0800

    Added intro codes for exercise 1

--- Exercise 1 - Temperature Conversion/temperature_conversion.py
+++ Exercise 1 - Temperature Conversion/temperature_conversion.py
@@ -6,7 +6,13 @@
 # Create a program that converts temperatures between Celsius and Fahrenheit.
 
 # Ask the user for input of temperature
+print("Welcome to the Temperature Converter!")
+temp_value = float(input("Please input the value of the Temperature: "))
+
 # Ask the user to select conversion type from Celsius to Fahrenheit or from Fahrenheit to Celsius
+print("\nPlease select a conversion type")
+conversion_type = input("Type 'C' to convert from Celsius to Fahrenheit or type 'F' to convert from Fahrenheit to Celsius: ")
+
 # Conversion
 # Output the result
 
